6a269446dfa3038b15faa06d0d7c1f107dc4d044
Renato Anjos <renatoanjos.ccs@gmail.com>
2018-12-19 12:44:39 +0000

    html -> php

---
+++ COISAS/scripts/teste.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Qual seu nome?"
+read nome
+echo "Qual a sua idade?"
+read idade
+
+echo "Meu nome é $nome e tenho $idade anos"
---
+++ pages/menu.php
@@ -0,0 +1,3 @@
+<div>
+    <input type="password" name="name_password"/>
+</div>
